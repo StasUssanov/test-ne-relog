2c23e882d8d771262dbaeb50211b69ef3fbd2ef8
StasUssanov <s.ussanov@mail.ru>
2021-12-21 18:05:42 +0600

    Добавить ant.design

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { Layout } from 'antd';
+
+const App: FC = () => (
+  <Layout>
+    <Layout.Content>Content</Layout.Content>
+  </Layout>
+);
+
+export default App;
